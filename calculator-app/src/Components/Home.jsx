e1aab7c21b9d47c90e93a0d46ab3b74a309c5c69
UshnaF <ushnafaheem559@gmail.com>
2023-12-16 17:40:44 +0500

    Adding Home page

---
+++ calculator-app/src/Components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+const Home = () => {
+    return (
+
+        <div>
+            <h3>WELCOME TO OUR PAGE👋</h3>
+            <p>
+                Welcome to Online Calculator! We have a range of free,
+                easy to use calculators, conversion tools, and much more!
+                Our tools are designed to help you perform a
+                wide range of calculations and conversions quickly and accuratel
+                y, whether you're at home, at work, or on the go.
+                Welcome to Online Calculator! We have a range of free,
+                easy to use calculators, conversion tools, and much more!
+                Our tools are designed to help you perform a
+                wide range of calculations and conversions quickly and accuratel
+                y, whether you're at home, at work, or on the go.
+            </p>
+            <p>
+            Welcome to Online Calculator! We have a range of free,
+                easy to use calculators, conversion tools, and much more!
+                Our tools are designed to help you perform a
+                wide range of calculations and conversions quickly and accuratel
+                y, whether you're at home, at work, or on the go.Welcome to Online Calculator! We have a range of free,
+                easy to use calculators, conversion tools, and much more!
+                Our tools are designed to help you perform a
+                wide range of calculations and conversions quickly and accuratel
+                y, whether you're at home, at work, or on the go.
+            </p>
+        </div>
+    )
+}
+
+export default Home
