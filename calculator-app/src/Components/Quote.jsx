c79463983f0523bbe7fa76b70638558efb0db7fd
UshnaF <ushnafaheem559@gmail.com>
2023-12-16 17:41:29 +0500

    Creating a Quotes page for Math magician

---
+++ calculator-app/src/Components/Quote.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Quote = () => {
+    return (
+        <div>
+            <h3>Quotes❞</h3>
+            <p> “Math is the only place where truth and beauty mean the same thing.”</p>
+            <b className='new'>— Danica McKellar</b>
+
+            <p>“The only way to learn mathematics is to do mathematics.”
+            </p>
+            <b className='new'>— Paul Halmos</b>
+
+            <p>“Figures don’t lie, but liars figure.”
+            </p>
+            <b className='new'>  — Mark Twain</b>
+
+        </div>
+    )
+}
+
+export default Quote
